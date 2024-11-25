447cb8e01fbab0389b6efb0338451590a8756271
aguilardaniel <agdaniel89@gmail.com>
2024-11-24 23:27:33 -0300

    Proyecto Final

--- cotizar.js
+++ cotizar.js
@@ -8,15 +8,15 @@ más le sirva según sus necesidades.
 Hay tras planes con distintas características.
 
 Plan A ("Inicial"):
-    - Hasta 30 productos
+    - Hasta 30 productos (si o si)
     - Sin soporte personalizado
 
 Plan B ("Medium"): 
-    - Productos ilimitados
+    - Con distintas versiones (hasta 150, hasta 500, hasta 3000 o ilimitados productos) y distintos descuentos en cada uno
     - Sin soporte personalizado
 
 Plan C ("Premium"):
-    - Productos ilimitados
+    - Con distintas versiones (hasta 30, hasta 150, hasta 500, hasta 3000 o ilimitados productos) y distintos descuentos en cada uno
     - Soporte via WhatsApp 
 
 
@@ -42,13 +42,42 @@ let recomendacion = document.getElementById("recomendacion");
 let cantidadProductosHtml= document.getElementById("cantidadProductos");
 let requiereSoporteHtml= document.getElementById("requiereSoporte");
 let labelCantidad= document.getElementById("labelCantidad");
+let listaDePlanesCotizados = document.getElementById("listaDePlanesCotizados");
 
 let botonVerHtml= document.getElementById("botonVer");
 let cotizarHtml= document.getElementById("cotizar");
-let cantidadString="lalala";
+let cantidadString="";
 let soportePersonalizado;
 let cantidadDeProductos;
-console.log(cantidadString);
+
+let planYaCotizado;
+let planesCotizados=[];
+
+
+const respuestaDelLocalStorage = (clave)=>{
+
+    return new Promise((resolve, reject) => {
+
+        /* if(JSON.parse(localStorage.getItem("Cotizaciones"))){
+            planesCotizados= JSON.parse(localStorage.getItem("Cotizaciones"));
+            } */
+            
+            respuesta= JSON.parse(localStorage.getItem(clave));
+            
+
+        if(respuesta && respuesta.length!==0){
+            console.log(respuesta);
+            resolve(respuesta);
+
+        }else{
+            console.log(respuesta);
+            reject("No cotizaste ningún plan aún");
+
+        }
+
+    })
+
+}
 
 
 
@@ -60,7 +89,16 @@ const planes=[
     {nombre:"Plan Premium", precio: 125000},
 ];
 
-localStorage.setItem("planes", JSON.stringify(planes));
+
+/* 
+if(JSON.parse(localStorage.getItem("Cotizaciones"))){
+    planesCotizados= JSON.parse(localStorage.getItem("Cotizaciones"));
+    } */
+
+
+
+
+//localStorage.setItem("planes", JSON.stringify(planes));
 //const planesJSON= JSON.stringify(planes);
 //localStorage.setItem(planesJSON);
 
@@ -78,7 +116,35 @@ let descuentoExtra;
 let ofertaCompleta="";
 
 
+
+const limiteDeProductos1= 30;
+const limiteDeProductos2= 150;
+const limiteDeProductos3= 500;
+const limiteDeProductos4= 3000;
+
+//const fecha = luxon.DateTime.now();
+const DateTime = luxon.DateTime;
+
+
+
+
 //----- Funciones--------
+function tomarFechaActual(){
+    const now= DateTime.now();
+//const fecha2= fecha.toLocalString(fecha.DATETIME_SHORT_WITH_SECONDS);
+//fecha=now.toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS);
+const fechaActual = now.toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS);
+return fechaActual;
+}
+
+function filtrarElementosViejos(){
+//console.log(planesCotizados[0].tiempo);
+//console.log(typeof planesCotizados[0].tiempo);
+   planesCotizados = planesCotizados.filter((planCotizado) => planCotizado.tiempo < 1);
+   guardarPlanes();
+   //localStorage.setItem("Cotizaciones" , JSON.stringify(planesCotizados));
+
+}
 
 function generarDescuentos(plan){
     
@@ -89,33 +155,205 @@ function generarDescuentos(plan){
 }
 
 
+function guardarPlanes(){
+
+    localStorage.setItem("Cotizaciones" , JSON.stringify(planesCotizados));
+
+}
+
+function mostrarCotizacionesAnteriores(){
+
+   /*  if(JSON.parse(localStorage.getItem("Cotizaciones"))){
+        planesCotizados= JSON.parse(localStorage.getItem("Cotizaciones"));
+        } */
+        
+
+    respuestaDelLocalStorage("Cotizaciones")
+        .then((resp)=>{
+
+            planesCotizados= resp;
+
+        })
+        .catch((resp)=>{
+
+            listaDePlanesCotizados.innerHTML+= `
+        <p><b>${resp}</b> </p>
+        <hr>`;
+
+
+        })
+
+    listaDePlanesCotizados.innerHTML= ``;
+
+    planesCotizados.forEach(planCotizado => {
+
+        actualizarTiempoRestante(planCotizado);
+        
+        
+        //Ejecuta la funcion para actualizar el tipo restante disponible de las cotizaciones ya hechas
+        
+        /* listaDePlanesCotizados.innerHTML+= `
+        
+        <h3>${planCotizado.plan}</h3>
+        <p><b>¿Incluye Soporte via WhatsApp?: </b> ${planCotizado.SoporteViaWhatsApp}</p>
+        <p><b>Cantidad Maxima de productos:</b> ${planCotizado.maximoDeProductos}</p>
+        <p><b>Valor:</b> $${planCotizado.precio}</p>
+        <p><b>Cotizado el día: </b>${ planCotizado.fecha}hs</p>
+        <button type="button" class="btn btn-success">Lo Quiero...</button>
+        <hr>`; */
+        
+    });
+ 
+
+    filtrarElementosViejos();
+
+
+    planesCotizados.forEach(planCotizado => {
+
+       /*  actualizarTiempoRestante(planCotizado); */
+        
+        
+        //Ejecuta la funcion para actualizar el tipo restante disponible de las cotizaciones ya hechas
+        
+        listaDePlanesCotizados.innerHTML+= `
+        
+        <h3>${planCotizado.plan}</h3>
+        <p><b>¿Incluye Soporte via WhatsApp?: </b> ${planCotizado.SoporteViaWhatsApp}</p>
+        <p><b>Cantidad Maxima de productos:</b> ${planCotizado.maximoDeProductos}</p>
+        <p><b>Valor:</b> $${planCotizado.precio}</p>
+        <p><b>Cotizado el día: </b>${ planCotizado.fecha}hs</p>
+        <button type="button" class="btn btn-success">Lo Quiero...</button>
+        <hr>`;
+        
+    });
+    //Y despues filtra los elementos que hace más de un mes fueron cotizados
+
+   /*  recomendacion.innerHTML= `<p class="ml-3"> Te sugerimos contratar nuestro <strong> ${planes[plan].nombre} </strong><br><br> 
+                Para la cantidad de productos que definiste <br>podemos hacerte <strong> un descuento de  ${descuentoTotalAplicado}% </strong> en el plan anual.
+                <br><br> Su valor final es de: <strong> $${valorFinalConDescuentos} por año </strong></p>`;
+    */
+}
+
+
 function mostrarDescuentos(plan){
+
 
     ofertaCompleta += "- " + planes[plan].nombre + " = $" + planesConDescuento[plan] + "<br>";
     
 
 }
 
+function actualizarTiempoRestante(planCotizado){
+    end = DateTime.fromFormat(tomarFechaActual(), "dd/MM/yyyy, HH:mm:ss");
+    start = DateTime.fromFormat(planCotizado.fecha, "dd/MM/yyyy, HH:mm:ss");
+    let diff = end.diff(start); // default unit is milliseconds
+    let diferenciaEnMeses = diff.as('months');
+    
+    planCotizado.tiempo = diferenciaEnMeses;
+    guardarPlanes();
+    //localStorage.setItem("Cotizaciones" , JSON.stringify(planesCotizados));
+
+
+    
+    /* if(diferenciaEnMeses>= 1){
+        //planesCotizados.pop();
+        localStorage.setItem("Cotizaciones" , JSON.stringify(planesCotizados));
+        
+    }else{
+        planCotizado.tiempo = diferenciaEnMeses;
+        localStorage.setItem("Cotizaciones" , JSON.stringify(planesCotizados));
+    }
+     */
+
+}
 
 function generarDescuentoFinal(plan){
-    
-    if(cantidadDeProductos<=30){
+    let limiteActual="Ilimitados";
+
+    if(cantidadDeProductos<=limiteDeProductos1){
         descuentoExtra=20;
+        limiteActual= limiteDeProductos1;
     }
-    else if(cantidadDeProductos>30 && cantidadDeProductos<=150){
+    else if(cantidadDeProductos>limiteDeProductos1 && cantidadDeProductos<=limiteDeProductos2){
         descuentoExtra=15;
+        limiteActual= limiteDeProductos2;
     }
-    else if(cantidadDeProductos>150 && cantidadDeProductos<=500){
+    else if(cantidadDeProductos>limiteDeProductos2 && cantidadDeProductos<=limiteDeProductos3){
         descuentoExtra=10;
+        limiteActual= limiteDeProductos3;
     }
-    else if(cantidadDeProductos>500 && cantidadDeProductos<=3000){
+    else if(cantidadDeProductos>limiteDeProductos3 && cantidadDeProductos<=limiteDeProductos4){
         descuentoExtra=5;
+        limiteActual= limiteDeProductos4;
     }else{
         descuentoExtra=0;
     }
 
     let precioConDescuento = planes[plan].precio - planes[plan].precio * (descuentoPorcentaje + descuentoExtra) / 100;
+    
+
+    //const fechaActual = new Date().toISOString();
+    //const fechaActual = now.toISO();
+    //console.log(fechaActual);
+    
+   
+
+    let cotizacionActual={
+        plan: planes[plan].nombre,
+        SoporteViaWhatsApp: requiereSoporte===true?"Si":"No",
+        maximoDeProductos: limiteActual,
+        precio: precioConDescuento,
+        fecha:  tomarFechaActual(),
+        tiempo: 0,
+    };
+
+
+
+
+    planesCotizados.forEach(planCotizado => {
+
+        
+       actualizarTiempoRestante(planCotizado);
+       
+        //Ejecuta la función para actualizar el tipo restante disponible de las cotizaciones ya hechas
+        
+        if(planCotizado.plan===cotizacionActual.plan 
+            && planCotizado.maximoDeProductos=== cotizacionActual.maximoDeProductos
+            && planCotizado.precio === cotizacionActual.precio)
+        {
+
+            
+            planYaCotizado=true;
+            planCotizado.fecha=  tomarFechaActual();
+            guardarPlanes();
+            
+            //localStorage.setItem("Cotizaciones" , JSON.stringify(planesCotizados));
+            mostrarCotizacionesAnteriores();
+
+        }
+        
+    });
+    
+
+    
+    if(!planYaCotizado===true){
+
+    planesCotizados.unshift(cotizacionActual);
+    guardarPlanes();
+    //localStorage.setItem("Cotizaciones" , JSON.stringify(planesCotizados));
+    mostrarCotizacionesAnteriores();
+
+    }
+    
+
+    filtrarElementosViejos();
+    //y despues filtra los elementos que hace más de un mes fueron cotizados
+      
+    
+    
     return precioConDescuento;
+
+
 
 }
 
@@ -139,7 +377,9 @@ function generarDescuentosYMostrar(vectorPlanes, funcionDefinida){
     
     /* La función de orden superior definida puede usarse con:
     generarDescuentos o mostrarDescuentos */
-    
+    ofertaCompleta="";
+    //Reinicia los datos de ofertaCompleta para evitar que le queden datos anteriores guardados
+
     for (let i = 0; i < vectorPlanes.length; i++) {
        funcionDefinida(i);
       }
@@ -154,15 +394,15 @@ function generarDescuentosYMostrar(vectorPlanes, funcionDefinida){
 
 //----- Fin Funciones -----
 
+mostrarCotizacionesAnteriores();
 
 cotizarHtml.addEventListener("click", (event)=>{
 
     event.preventDefault();
+    planYaCotizado=false;
     //operador logico para agregar la clase oculto a un boton en caso de que no la contenga
     !botonVerHtml.classList.contains('oculto')&&botonVerHtml.classList.add('oculto');
-    console.log("entro al evento");
     cantidadString = cantidadProductosHtml.value;
-    console.log(cantidadString);
     soportePersonalizado = requiereSoporteHtml.value;
 
     //Evalúa cuantos productos tiene
@@ -181,8 +421,8 @@ cotizarHtml.addEventListener("click", (event)=>{
             
             //Muestra una advertencia para que se ingrese un numero válido en el form y sale de la ejecución de la función
             
-            recomendacion.innerHTML= `<p class="bg-warning"><strong>Ingresá un número válido</strong> para generarte un descuento</p>
-            <p>O mirá nuestros planes <strong>sin descuentos</strong></p> <br> 
+            recomendacion.innerHTML= `<div class="mb-0"><p class="bg-warning"><strong>Ingresá un número válido</strong> para generarte un descuento</p>
+            <p>O mirá nuestros planes <strong>sin descuentos</strong></p> <br> <div>
             `;
             
             botonVerHtml.classList.remove('oculto');
@@ -268,9 +508,12 @@ cotizarHtml.addEventListener("click", (event)=>{
 
 botonVerHtml.addEventListener("click", (event)=>{
 
-    recomendacion.innerHTML= `<p><strong>Te sugerimos que nos contactes por WhatsApp</strong><br>
-    para ayudarte a definir el mejor plan para vos<br><br>${ofertaCompleta} <br>
-    Nuestros vendedores podrán asignarte descuentos extras <br>
+    recomendacion.innerHTML= `<div>
+    <p class="mb-0"><strong>Te sugerimos que nos contactes por WhatsApp</strong></p>
+    <p>para ayudarte a definir el mejor plan para vos</p>
+    <div>
+    <br>${ofertaCompleta}<br>
+    <p>Nuestros vendedores podrán asignarte descuentos extras <br>
     dependiendo de la cantidad de productos que tengas </p>`;
     botonVerHtml.classList.add('oculto');
     
